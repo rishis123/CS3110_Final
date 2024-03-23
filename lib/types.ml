1fa478ade246dc627b8a76a23e29e600321d3594
Zhijia Ye <zy424@cornell.edu>
2024-03-23 18:05:08 -0400

    Initialize project with Types

---
+++ lib/types.ml
@@ -0,0 +1,4 @@
+type unencryptable = MasterPasswordHash of string
+type encryptable = 
+Password of { name: string; password: string }
+(* | Login of { name: string; username: string; password: string; url: string option } *)
