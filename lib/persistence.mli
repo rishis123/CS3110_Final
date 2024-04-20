d3326422f4239fe08184f5518c04fc4ea7eee137
Rohen Giralt <rmg296@cornell.edu>
2024-04-20 14:59:10 -0400

    Fix master_password_hash type

--- lib/persistence.mli
+++ lib/persistence.mli
@@ -1,4 +1,4 @@
-val read_master_password_hash : unit -> Types.masterPasswordHash
+val read_master_password_hash : unit -> Types.master_password_hash
 val write_unencryptable : Types.unencryptable -> unit
 val read_all_encryptable : unit -> Types.encryptable list
 val write_encryptable : Types.encryptable -> unit
