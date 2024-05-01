a9829ca3663d6b62f87cd34f44fd34906d41a28e
Zhijia Ye <zy424@cornell.edu>
2024-04-30 23:47:06 -0400

    Added Add Password view
    - Added button transition from home view to add view

--- bin/gui.ml
+++ bin/gui.ml
@@ -7,7 +7,15 @@ open FinalProject
 let btn_font_size = 18
 let btn_border_radius = 10
 let window_width = 400
-let controller = W.empty ~w:0 ~h:0 ()
+let login_signal = W.empty ~w:0 ~h:0 ()
+let add_signal = W.empty ~w:0 ~h:0 ()
+
+let create_connection signal layout new_view =
+  W.connect_main signal signal
+    (fun _ _ _ ->
+      L.set_rooms layout [ new_view ];
+      Sync.push (fun () -> L.fit_content ~sep:0 layout))
+    [ Trigger.update ]
 
 let create_btn s f =
   W.button
@@ -16,36 +24,48 @@ let create_btn s f =
     ~action:(fun _ -> f ())
     ""
 
-(** [trasition_home_view layout] changes the view to the one shown when the user
-    logs in. *)
-let transition_home_view layout =
-  let label = W.label ~size:40 "You are signed in!" in
-  let new_layout =
-    L.tower
-      [
-        L.resident ~w:window_width ~h:200 label;
-        L.resident ~w:window_width
-          (create_btn "Add a password" (fun () ->
-               print_endline "add btn pressed"));
-        L.resident ~w:window_width
-          (create_btn "List passwords" (fun () ->
-               print_endline "list btn pressed"));
-        L.resident ~w:window_width
-          (create_btn "Generate a password" (fun () ->
-               print_endline "gen btn pressed"));
-        L.resident ~w:window_width
-          (create_btn "Set the master password" (fun () ->
-               print_endline "setpwd btn pressed"));
-        L.resident ~w:window_width
-          (create_btn "Import passwords" (fun () ->
-               print_endline "import btn pressed"));
-        L.resident ~w:window_width
-          (create_btn "Export a password" (fun () ->
-               print_endline "Export btn pressed"));
-      ]
+let add_view =
+  let name_input =
+    W.text_input ~size:30 ~max_size:200 ~prompt:"Enter password name" ()
   in
-  L.set_rooms layout [ new_layout ];
-  Sync.push (fun () -> L.fit_content ~sep:0 layout)
+  let pwd_input =
+    W.text_input ~size:30 ~max_size:200 ~prompt:"Enter password" ()
+  in
+  let add_btn =
+    create_btn "Add password" (fun () -> print_endline "add password pressed")
+  in
+  L.tower
+    [
+      L.resident ~w:window_width name_input;
+      L.resident ~w:window_width pwd_input;
+      L.resident ~w:window_width ~h:200 add_btn;
+    ]
+
+let home_view =
+  let label = W.label ~size:40 "You are signed in!" in
+  let add_btn =
+    create_btn "Add a password" (fun () -> Update.push add_signal)
+  in
+  L.tower
+    [
+      L.resident ~w:window_width ~h:200 label;
+      L.resident ~w:window_width add_btn;
+      L.resident ~w:window_width
+        (create_btn "List passwords" (fun () ->
+             print_endline "list btn pressed"));
+      L.resident ~w:window_width
+        (create_btn "Generate a password" (fun () ->
+             print_endline "gen btn pressed"));
+      L.resident ~w:window_width
+        (create_btn "Set the master password" (fun () ->
+             print_endline "setpwd btn pressed"));
+      L.resident ~w:window_width
+        (create_btn "Import passwords" (fun () ->
+             print_endline "import btn pressed"));
+      L.resident ~w:window_width
+        (create_btn "Export a password" (fun () ->
+             print_endline "Export btn pressed"));
+    ]
 
 (** [login_view] is the view shown before the user logs in *)
 let login_view =
@@ -58,26 +78,24 @@ let login_view =
         let pwd = W.get_text input in
         if MasterPassword.check_master pwd then
           let () = Encrypt.set_key pwd in
-          Update.push controller
+          Update.push login_signal
         else W.set_text label "Password incorrect")
   in
-  let layout =
-    L.tower
-      [
-        L.resident ~w:window_width input;
-        L.resident ~w:window_width login_btn;
-        L.resident ~w:window_width ~h:200 label;
-      ]
-  in
-  let c =
-    W.connect_main controller controller
-      (fun _ _ _ -> transition_home_view layout)
-      [ Trigger.update ]
-  in
-  Bogue.of_layout ~connections:[ c ] layout
+  L.tower
+    [
+      L.resident ~w:window_width input;
+      L.resident ~w:window_width login_btn;
+      L.resident ~w:window_width ~h:200 label;
+    ]
+
+let connections =
+  [
+    create_connection login_signal login_view home_view;
+    create_connection add_signal home_view add_view;
+  ]
 
 let main () =
-  let board = login_view in
+  let board = Bogue.of_layout ~connections login_view in
   Bogue.run board
 
 let _ =
