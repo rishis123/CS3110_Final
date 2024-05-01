76ccd85dd5748e4cb06118b38a2bfe952b8ff34a
Zhijia Ye <zy424@cornell.edu>
2024-05-01 11:45:53 -0400

    Implemented Gui password adding feature
    - Gui has add password view
    - Gui interfaces with persistence to receive input and store passwords
    - Gui has a action complete view that has a button that goes back to the home view

--- bin/gui.ml
+++ bin/gui.ml
@@ -7,12 +7,14 @@ open FinalProject
 let btn_font_size = 18
 let btn_border_radius = 10
 let window_width = 400
+let label_text_size = 18
 
 (** Buttons that change views need "signal" widgets, which are empty widgets
     used to recieve Update.push signals to trigger view changes. *)
 let login_signal = W.empty ~w:0 ~h:0 ()
 
 let add_signal = W.empty ~w:0 ~h:0 ()
+let add_complete_signal = W.empty ~w:0 ~h:0 ()
 
 (** [create_connection signal old_view new_view] creates a connection such that
     when [Update.push signal] is called, the view changes from [oldview] to
@@ -34,8 +36,20 @@ let create_btn s f =
     ~action:(fun _ -> f ())
     ""
 
+(** [action_complete_view msg] is a view with the message [msg] and a back
+    button which trasitions to [home_view]. Displayed after an action is
+    completed. *)
+let action_complete_view msg =
+  let label = W.label ~size:label_text_size msg in
+  let back_btn =
+    create_btn "Back to home" (fun () -> print_endline "Go back home")
+  in
+  L.tower
+    [ L.resident ~w:window_width label; L.resident ~w:window_width back_btn ]
+
 (** [add_view] is the view shown when the user adds a new password. *)
 let add_view =
+  let label = W.label ~size:label_text_size "Enter information" in
   let name_input =
     W.text_input ~size:30 ~max_size:200 ~prompt:"Enter password name" ()
   in
@@ -43,18 +57,28 @@ let add_view =
     W.text_input ~size:30 ~max_size:200 ~prompt:"Enter password" ()
   in
   let add_btn =
-    create_btn "Add password" (fun () -> print_endline "add password pressed")
+    create_btn "Add password" (fun () ->
+        let name = W.get_text name_input in
+        let pwd = W.get_text pwd_input in
+        if String.length name > 0 && String.length pwd > 0 then (
+          let encryptable =
+            FinalProject.Types.Password { name; password = pwd }
+          in
+          FinalProject.Persistence.write_encryptable encryptable;
+          Update.push add_complete_signal)
+        else W.set_text label "Fields must not be empty")
   in
   L.tower
     [
       L.resident ~w:window_width name_input;
       L.resident ~w:window_width pwd_input;
-      L.resident ~w:window_width ~h:200 add_btn;
+      L.resident ~w:window_width add_btn;
+      L.resident ~w:window_width ~h:200 label;
     ]
 
 (** [home_view] is the view shown right after the user logs in. *)
 let home_view =
-  let label = W.label ~size:40 "You are signed in!" in
+  let label = W.label ~size:label_text_size "You are signed in!" in
   let add_btn =
     create_btn "Add a password" (fun () -> Update.push add_signal)
   in
@@ -84,7 +108,7 @@ let login_view =
   let input =
     W.text_input ~size:30 ~max_size:200 ~prompt:"Enter your name" ()
   in
-  let label = W.label ~size:40 "Hello! Please log in" in
+  let label = W.label ~size:label_text_size "Hello! Please log in" in
   let login_btn =
     create_btn "Login" (fun () ->
         let pwd = W.get_text input in
@@ -106,6 +130,8 @@ let connections =
   [
     create_connection login_signal login_view home_view;
     create_connection add_signal home_view add_view;
+    create_connection add_complete_signal add_view
+      (action_complete_view "Password added");
   ]
 
 (** [main ()] is Bogue's main loop. It will display this board until the window
