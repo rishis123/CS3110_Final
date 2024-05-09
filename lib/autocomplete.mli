3f6d37a1d6a5d371ac33be16b32a3adb6863c934
Rishi Shah <rrs244@cornell.edu>
2024-05-09 17:28:10 -0400

    Finished autocomplete testing

--- lib/autocomplete.mli
+++ lib/autocomplete.mli
@@ -2,3 +2,4 @@ val autocomplete : string -> Types.encryptable list
 val check_strength : string -> bool
 val compare_words : string -> string -> bool
 val common_passwords : string list
+val check_vulnerabilities : unit -> string list
