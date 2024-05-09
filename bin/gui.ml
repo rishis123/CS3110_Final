1e0fc36d1e11ee1649cda5d47d112d0dcefebb3e
Zhijia Ye <zy424@cornell.edu>
2024-05-08 22:36:08 -0400

    Add list view
    - List button on home transitions to list view
    - Add view has cancel button

--- bin/gui.ml
+++ bin/gui.ml
@@ -19,6 +19,7 @@ let login_signal = W.empty ~w:0 ~h:0 ()
 let add_signal = W.empty ~w:0 ~h:0 ()
 let add_complete_signal = W.empty ~w:0 ~h:0 ()
 let back_home_signal = W.empty ~w:0 ~h:0 ()
+let list_signal = W.empty ~w:0 ~h:0 ()
 
 (** [create_btn s f] creates a button with label text [s] that does [f] when
     clicked. Note: This function does not create a connection, which is
@@ -30,14 +31,15 @@ let create_btn s f =
     ~action:(fun _ -> f ())
     ""
 
+(** [back_btn] is a button that when pressed, returns to the home view. *)
+let back_btn =
+  create_btn "Back to home" (fun () -> Update.push back_home_signal)
+
 (** [action_complete_view msg] is a view with the message [msg] and a back
     button which trasitions to [home_view]. Displayed after an action is
     completed. *)
 let action_complete_view msg =
   let label = W.label ~size:label_text_size msg in
-  let back_btn =
-    create_btn "Back to home" (fun () -> Update.push back_home_signal)
-  in
   L.tower
     [ L.resident ~w:window_width label; L.resident ~w:window_width back_btn ]
 
@@ -59,16 +61,76 @@ let add_view =
             FinalProject.Types.Password { name; password = pwd }
           in
           FinalProject.Persistence.write_encryptable encryptable;
+          W.set_text name_input "";
+          W.set_text pwd_input "";
           Update.push add_complete_signal)
         else W.set_text label "Fields must not be empty")
+  in
+  let cancel_btn =
+    create_btn "Cancel" (fun () ->
+        W.set_text name_input "";
+        W.set_text pwd_input "";
+        Update.push back_home_signal)
   in
   L.tower
     [
       L.resident ~w:window_width name_input;
       L.resident ~w:window_width pwd_input;
       L.resident ~w:window_width add_btn;
+      L.resident ~w:window_width cancel_btn;
       L.resident ~w:window_width ~h:200 label;
     ]
+
+(** [list_view] is the view shown when the user lists all passwords. *)
+let list_view =
+  let pwd_list = Persistence.read_all_encryptable () in
+  let h = 50 in
+  let create_login_label = function
+    | Types.Password p ->
+        L.tower
+          [
+            L.resident ~w:window_width
+              (W.rich_text ~size:label_text_size ~h
+                 Text_display.(page [ bold (para "Name: "); para p.name ]));
+            L.resident ~w:window_width
+              (W.rich_text ~size:label_text_size ~h
+                 Text_display.(
+                   page [ bold (para "Password: "); para p.password ]));
+          ]
+    | Types.Login l -> (
+        let fields =
+          [
+            L.resident ~w:window_width
+              (W.rich_text ~size:label_text_size ~h
+                 Text_display.(page [ bold (para "Name: "); para l.name ]));
+            L.resident ~w:window_width
+              (W.rich_text ~size:label_text_size ~h
+                 Text_display.(
+                   page [ bold (para "Username: "); para l.username ]));
+            L.resident ~w:window_width
+              (W.rich_text ~size:label_text_size ~h
+                 Text_display.(
+                   page [ bold (para "Password: "); para l.password ]));
+          ]
+        in
+        match l.url with
+        | None -> L.tower fields
+        | Some url ->
+            L.tower
+              (fields
+              @ [
+                  L.resident ~w:window_width
+                    (W.rich_text ~size:label_text_size ~h
+                       Text_display.(page [ bold (para "Url: "); para url ]));
+                ]))
+  in
+  let label_list = List.map create_login_label pwd_list in
+  let scrollpane =
+    L.make_clip ~scrollbar:true ~h:(L.height add_view)
+      (L.tower
+         (label_list @ [ L.tower [ L.resident ~w:window_width back_btn ] ]))
+  in
+  L.tower [ scrollpane ]
 
 (** [home_view] is the view shown right after the user logs in. *)
 let home_view =
@@ -76,13 +138,12 @@ let home_view =
   let add_btn =
     create_btn "Add a password" (fun () -> Update.push add_signal)
   in
+  let list_btn = create_btn "List logins" (fun () -> Update.push list_signal) in
   L.tower
     [
       L.resident ~w:window_width ~h:200 label;
       L.resident ~w:window_width add_btn;
-      L.resident ~w:window_width
-        (create_btn "List passwords" (fun () ->
-             print_endline "list btn pressed"));
+      L.resident ~w:window_width list_btn;
       L.resident ~w:window_width
         (create_btn "Generate a password" (fun () ->
              print_endline "gen btn pressed"));
@@ -100,12 +161,13 @@ let home_view =
 (** [login_view] is the view shown before the user logs in *)
 let login_view =
   let input =
-    W.text_input ~size:30 ~max_size:200 ~prompt:"Enter your name" ()
+    W.text_input ~size:30 ~max_size:200 ~prompt:"Enter master password" ()
   in
   let label = W.label ~size:label_text_size "Hello! Please log in" in
   let login_btn =
     create_btn "Login" (fun () ->
         let pwd = W.get_text input in
+        W.set_text input "";
         if MasterPassword.check_master pwd then
           let () = Encrypt.set_key pwd in
           Update.push login_signal
@@ -138,6 +200,7 @@ let connections =
     create_connection add_complete_signal
       (action_complete_view "Password added");
     create_connection back_home_signal home_view;
+    create_connection list_signal list_view;
   ]
 
 (** [main ()] is Bogue's main loop. It will display this board until the window
