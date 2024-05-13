8a699bdc9de396a3bc05f5c975bbe9818395d698
Sophia Song <ss3579@cornell.edu>
2024-05-13 19:19:09 -0400

    Fixed password generation bug
    - generated passwords no longer consist of only one character

--- lib/gen_password.ml
+++ lib/gen_password.ml
@@ -1,34 +1,19 @@
-(* let rec generate_password_without_special_old index acc = if index <= 0 then
-   acc else let range_choice = Random.int 3 in let char_val = match range_choice
-   with | 0 -> Char.chr (97 + Random.int 26) (* lowercase letters (a-z) *) | 1
-   -> Char.chr (65 + Random.int 26) (* uppercase letters (A-Z) *) | _ ->
-   Char.chr (48 + Random.int 10) (* digits (0-9) *) in
-   generate_password_without_special_old (index - 1) (char_val :: acc) *)
-
 let generate_password_without_special length =
-  String.make length
-    (match Random.int 4 with
-    | 0 -> Char.chr (97 + Random.int 26) (* lowercase letters (a-z) *)
-    | 1 -> Char.chr (65 + Random.int 26) (* uppercase letters (A-Z) *)
-    | _ -> Char.chr (48 + Random.int 10) (* digits (0-9) *))
-
-(* let rec generate_password_with_special_old index acc = if index <= 0 then acc
-   else let range_choice = Random.int 4 in let char_val = match range_choice
-   with | 0 -> Char.chr (97 + Random.int 26) (* lowercase letters (a-z) *) | 1
-   -> Char.chr (65 + Random.int 26) (* uppercase letters (A-Z) *) | 2 ->
-   Char.chr (48 + Random.int 10) (* digits (0-9) *) | _ -> Char.chr (Array.get
-   [| 35; 36; 38 |] (Random.int 3)) (* symbols (#, $, &) *) in
-   generate_password_with_special_old (index - 1) (char_val :: acc) *)
+  String.init length (fun _ ->
+      match Random.int 4 with
+      | 0 -> Char.chr (97 + Random.int 26) (* lowercase letters (a-z) *)
+      | 1 -> Char.chr (65 + Random.int 26) (* uppercase letters (A-Z) *)
+      | _ -> Char.chr (48 + Random.int 10)
+      (* digits (0-9) *))
 
 let generate_password_with_special length =
-  String.make length
-    (match Random.int 4 with
-    | 0 -> Char.chr (97 + Random.int 26) (* lowercase letters (a-z) *)
-    | 1 -> Char.chr (65 + Random.int 26) (* uppercase letters (A-Z) *)
-    | 2 -> Char.chr (48 + Random.int 10) (* digits (0-9) *)
-    | _ ->
-        Char.chr (Array.get [| 35; 36; 38 |] (Random.int 3))
-        (* symbols (#, $, &) *))
+  String.init length (fun _ ->
+      match Random.int 4 with
+      | 0 -> Char.chr (97 + Random.int 26) (* lowercase letters (a-z) *)
+      | 1 -> Char.chr (65 + Random.int 26) (* uppercase letters (A-Z) *)
+      | 2 -> Char.chr (48 + Random.int 10) (* digits (0-9) *)
+      | _ -> Char.chr (Array.get [| 35; 36; 38 |] (Random.int 3))
+      (* symbols (#, $, &) *))
 
 let gen_password_val () =
   print_endline "Choose password length:";
