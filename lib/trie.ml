1c8527e4098d49c759f0c10cb60bf1fea247f4c9
Zhijia Ye <zy424@cornell.edu>
2024-05-16 00:56:49 -0400

    Add Test Plan
    - Added Test Plan.pdf to root directory
    - Fixed typo in trie.ml comment

--- lib/trie.ml
+++ lib/trie.ml
@@ -8,7 +8,7 @@ type t = node OrderedMultiset.t
 (** The type of a trie that stores strings. AF:
     [node {value=c; children=s; is_terminal=b}] is the trie containing all
     strings beginning with [c] followed by any string contained in the subtree
-    rooted at each of its children. If [is_termina] is false, [children] is the
+    rooted at each of its children. If [is_terminal] is false, [children] is the
     set of all of a node's children. If [is_terminal] is true, then a node has
     no children regardless of the value of [children]. The [OrderedMultiset]
     containing only [None] is the empty trie. RI: None *)
