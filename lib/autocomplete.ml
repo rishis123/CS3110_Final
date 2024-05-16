69bb9d938884d3f344bac782edbf18a884a5c24f
Rishi Shah <rrs244@cornell.edu>
2024-05-15 21:39:12 -0400

    Fixed autocomplete bugs

--- lib/autocomplete.ml
+++ lib/autocomplete.ml
@@ -17,28 +17,36 @@ let clean_up str =
   in
   clean_chars 0
 
+let get_substrings str =
+  let len = String.length str in
+  let rec generate_substrings i =
+    if i + 3 > len then [] else String.sub str i 3 :: generate_substrings (i + 1)
+  in
+  generate_substrings 0
+
+let check_substring sub_str str =
+  if String.length sub_str < 3 then false
+  else if String.length sub_str > String.length str then false
+  else
+    let len = String.length sub_str in
+    let rec compare_chars i =
+      if i >= len then true
+      else if sub_str.[i] <> str.[i] then false
+      else compare_chars (i + 1)
+    in
+    compare_chars 0 && len >= 3
+
+let rec iterate substrings str =
+  match substrings with
+  | [] -> false
+  | sub_str :: rest ->
+      if check_substring sub_str str then true else iterate rest str
+
 let compare_words str1 str2 =
-  let str1 = clean_up str1 in
-  let str2 = clean_up str2 in
-  let len1 = String.length str1 in
-  let len2 = String.length str2 in
-  let rec check_overlap i1 i2 count =
-    if i1 >= len1 || i2 >= len2 then count >= 3
-    else if str1.[i1] = str2.[i2] then
-      check_overlap (i1 + 1) (i2 + 1) (count + 1)
-    else check_overlap (i1 + 1) 0 0
-  in
-  let rec iterate i1 =
-    if i1 >= len1 then false
-    else
-      let rec iterate_inner i2 =
-        if i2 >= len2 then iterate (i1 + 1)
-        else if check_overlap i1 i2 0 then true
-        else iterate_inner (i2 + 1)
-      in
-      iterate_inner 0
-  in
-  iterate 0
+  let new_str1 = clean_up str1 in
+  let substrings = get_substrings new_str1 in
+  let new_str2 = clean_up str2 in
+  iterate substrings new_str2
 
 let autocomplete seek_word =
   let encryptable_list = Persistence.read_all_encryptable () in
